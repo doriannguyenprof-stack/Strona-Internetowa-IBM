e3b615700ce67bcf6005a3d4cda690f145c88d46
Julles782 <s208739@student.pg.edu.pl>
2026-01-12 21:39:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,22 +8,54 @@ const productData = json.products;
 // Wybór elementu z elementami katalogu
 const catalogueItemsContainer = document.getElementById("catalogue-items");
 
-// Dla każdego produktu tworzymy element katalogu
-productData.forEach((product) => {
-	// Tworzymy element 'div'
-	const div = document.createElement("div");
+const productTypeSelect = document.getElementById("type-select");
+productTypeSelect.addEventListener("input", () => {
+	showProducts();
+});
 
-	// Dodajemy klasę do elementu
-	div.classList = "product-card";
+productTypeSelect.value = "wszystko";
 
-	// Dodajemy wyświetlanie danych do elementu
-	div.innerHTML = `
+const nameSearch = document.getElementById("name-search");
+nameSearch.addEventListener("input", () => {
+	showProducts();
+});
+
+function showProducts() {
+	// Czyścimy katalog, by od nowa go zbudować
+	catalogueItemsContainer.innerHTML = "";
+
+	// Dla każdego produktu tworzymy element katalogu
+	productData.forEach((product) => {
+		if (
+			productTypeSelect.value != "wszystko" &&
+			product.type != productTypeSelect.value
+		) {
+			return;
+		}
+
+		if (
+			!product.name.toLowerCase().includes(nameSearch.value.toLowerCase())
+		) {
+			return;
+		}
+
+		// Tworzymy element 'div'
+		const div = document.createElement("div");
+
+		// Dodajemy klasę do elementu
+		div.classList = "product-card";
+
+		// Dodajemy wyświetlanie danych do elementu
+		div.innerHTML = `
         <div>Typ: ${product.type}</div>
         <img src="${product.image_url}" alt="product image"/>
-        <div>Cena: ${product.price_pln} zł</div>
+        <div>Nazwa: ${product.name}</div>
         <div>Opis: ${product.description}</div>
     `;
 
-	// Dodanie elementu do strony
-	catalogueItemsContainer.appendChild(div);
-});
+		// Dodanie elementu do strony
+		catalogueItemsContainer.appendChild(div);
+	});
+}
+
+showProducts();
